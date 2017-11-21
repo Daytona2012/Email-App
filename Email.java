c264bfb2b598ba4c622055e4f2bc0d598f76e240
Joel Chester <joel.chester@gmail.com>
2017-11-21 18:10:54 -0500

    Email App Code

---
+++ Email.java
@@ -0,0 +1,84 @@
+package emailApp;
+
+import java.util.Scanner;
+
+public class Email {
+	private String firstName;
+	private String lastName;
+	private String password;
+	private String department;
+	private String email;
+	private int mailboxCapacity = 500;
+	private int defaultPasswordLength = 10;
+	private String otherMail;
+	private String companySuffix = "DayneIndustries.com";
+	
+	//Constructor for first name and last name
+	public Email(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName  = lastName;
+		//System.out.println("EMAIL CREATED: " + this.firstName + " " + this.lastName);
+		
+		//Call a method asking for the department - return the department 
+		this.department = setDepartment();
+		//System.out.println("Department: " + this.department);
+		
+		//Call a method that returns a random password
+		this.password = randomPassword(defaultPasswordLength);
+		System.out.println("Your password is: " + this.password);
+		
+		//Combine elements to generate email
+		email = firstName.toLowerCase() + lastName.toLowerCase() + "@" + department + "." + companySuffix;
+		//System.out.println("Your email is: " + email);
+	}
+	
+	//Ask for Department
+	private String setDepartment() {
+		System.out.print("New worker: " + firstName + ". Department Codes\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter department code:");
+		Scanner in = new Scanner(System.in);
+		int deptChoice = in.nextInt();
+		
+		if (deptChoice == 1) {return "sales";}
+		else if (deptChoice == 2) {return "dev";}
+		else if (deptChoice == 3) {return "acct";}
+		else {return "";}
+			
+	}
+	
+	//Generate random password
+	private String randomPassword(int length) {
+		String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*";
+		char[] password = new char[length];
+		for(int i = 0; i < length; i++) {
+			int rand = (int)(Math.random() * passwordSet.length());
+			password[i] = passwordSet.charAt(rand);
+		}
+		return new String(password);
+	}
+	
+	//Set the mailbox capacity
+	public void setMailBoxCapacity(int capacity) {
+		this.mailboxCapacity = capacity;
+	}
+	
+	//Set the other email
+	public void setOtherMail(String othMail) {
+		this.otherMail = othMail;
+	}
+	//Change the email
+	public void changePassword (String password) {
+		this.password = password;
+	}
+	
+	//Accessing  get methods directly
+	public int getMailBoxCapacity() {return mailboxCapacity;}
+	public String getOtherMail() { return otherMail;}
+	public String getPassword() {return password;}
+	
+	public String showInfo() {
+		return "DISPLAY NAME: " + firstName + " " + lastName +
+			   "\nCOMPANY EMAIL: " + email +
+			   "\nMAILBOX CAPACITY: " + mailboxCapacity + "mb";
+	}
+
+}
